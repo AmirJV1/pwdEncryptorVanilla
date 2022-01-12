3ef6412f8952c32bc59811ee1fca80dc7fc2df5e
Amir Velasquez <amir.velasquez1@gmail.com>
2022-01-12 11:34:46 -0400

    added validation?

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const submitBtn = document.querySelector('.SubmitBtn');
 const encOutput = document.querySelector('.encOutput');
 const copyBtn = document.querySelector('.copyEnc1');
 const alert = document.querySelector('.alert');
+const secKeyInp = document.querySelector('.secret');
 
 const copyToClip = (copyText) => {
 	copyText.select(); /* Select the text field */
@@ -12,8 +13,18 @@ const copyToClip = (copyText) => {
 };
 
 submitBtn.addEventListener('click', () => {
-	const cipher = crypt.encrypt(encInput.value);
-	encOutput.value = cipher;
+	console.log(encInput.value);
+	if (encInput.value !== '') {
+		crypt.secret = secKeyInp.value;
+		const cipher = crypt.encrypt(encInput.value);
+		if (cipher !== '') {
+			encOutput.value = cipher;
+		} else {
+			encOutput.value = 'Not a valid input!';
+		}
+	} else {
+		encOutput.value = 'Not a valid input!';
+	}
 });
 copyBtn.addEventListener('click', () => {
 	alert.style.display = 'block';
@@ -28,10 +39,16 @@ const decSubmitBtn = document.querySelector('.decSubmitBtn');
 const decOutput = document.querySelector('.decOutput');
 const copyBtn2 = document.querySelector('.copyEnc2');
 const alert2 = document.querySelector('.alert2');
+const oldSec = document.querySelector('.oldSecret');
 
 decSubmitBtn.addEventListener('click', () => {
+	crypt.secret = oldSec.value;
 	const decipher = crypt.decrypt(decInput.value);
-	decOutput.value = decipher;
+	if (decipher !== '') {
+		decOutput.value = decipher;
+	} else {
+		decOutput.value = 'Not a valid key or secret!';
+	}
 });
 copyBtn2.addEventListener('click', (e) => {
 	alert2.style.display = 'block';
